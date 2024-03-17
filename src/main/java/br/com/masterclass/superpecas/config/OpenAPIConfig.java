bfdb5b185346fc663880a32d035f995e24d9e02b
Victor Lembo <victor.lembo@pagmob.com.br>
2024-03-17 10:43:41 -0300

    Swagger Config

--- src/main/java/br/com/masterclass/superpecas/config/OpenAPIConfig.java
+++ src/main/java/br/com/masterclass/superpecas/config/OpenAPIConfig.java
@@ -1,4 +1,21 @@
 package br.com.masterclass.superpecas.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("SuperPecas API")
+                        .description("API para o sistema Super Peças")
+                        .version("1.0.0")
+                        .contact(new Contact().name("Nome do Contato").email("contato@superpecas.com"))
+                        .license(new License().name("MIT").url("https://opensource.org/licenses/MIT")));
+    }
 }
